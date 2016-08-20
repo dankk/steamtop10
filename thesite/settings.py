0fbee9c32a769a626079c28df9957674e97643a5
Dan K <dan.khalid3@gmail.com>
2016-08-20 03:19:59 -0400

    heroku pls

--- thesite/settings.py
+++ thesite/settings.py
@@ -122,13 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
 # heroku stuff
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '/steam/static')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = ()
